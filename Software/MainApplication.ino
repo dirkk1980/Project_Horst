bca47d976a64c38b491b39cabbb46ed12b9acee4
dirk.koehler <dirk.koehler@cardioscan.de>
2018-06-28 09:20:39 +0200

    set up project to basic state

--- Software/MainApplication.ino
+++ Software/MainApplication.ino
@@ -1,21 +1,27 @@
 #include <Arduino.h>
-#include <Ultrasonic.h>
+#include <HCSR04.h>
+#include <DRV8871.h>
+#include <DRV8871Dual.h>
 
 //global debug flag
-#define DEBUG
+//#define DEBUG
 
-//pin configuration
-#define FRONT_SENSOR_TRIGGER_PIN   13
-#define FRONT_SENSOR_ECHO_PIN      12
-//sensor specifications
-#define SENSOR_MAX_DISTANCE        450
-#define SENSOR_MIN_DISTANCE        2
+//pin configuration ultrasonic front sensor
+#define FRONT_SENSOR_TRIGGER_PIN   14
+#define FRONT_SENSOR_ECHO_PIN      13
 
-Ultrasonic frontSensor (
-FRONT_SENSOR_TRIGGER_PIN,
-FRONT_SENSOR_ECHO_PIN,
-SENSOR_MAX_DISTANCE,
-SENSOR_MIN_DISTANCE);        
+//pin configuration motor left
+#define MOTOR_LEFT_IN_1            12
+#define MOTOR_LEFT_IN_2            11
+
+//pin configuration motor right
+#define MOTOR_RIGHT_IN_1           10
+#define MOTOR_RIGHT_IN_2            9
+
+HCSR04 frontSensor (FRONT_SENSOR_TRIGGER_PIN, FRONT_SENSOR_ECHO_PIN);
+DRV8871 motor_left (MOTOR_LEFT_IN_1, MOTOR_LEFT_IN_2);
+DRV8871 motor_right (MOTOR_RIGHT_IN_1, MOTOR_RIGHT_IN_2);
+DRV8871Dual motorController (&motor_left, &motor_right);
 
 void setup() {                          
   #ifdef DEBUG
@@ -26,9 +32,7 @@ void setup() {
 
 void loop() {
   #ifdef DEBUG
-  Serial.println("\nMeasured distance:");
-  Serial.print(frontSensor.getDistanceCM());
-  Serial.print(" cm");
+    Serial.println("----");
   #endif
   //wait one second
   delay(1000);
